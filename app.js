87bd5257a484d3d966913b1b950d96885f37451a
Ben Bryant <brbryant2639@gmail.com>
2020-02-07 18:24:44 -0600

    adds h2 with inputted value once submitted

--- app.js
+++ app.js
@@ -8,10 +8,27 @@ $(function(){
         }
     })
 });
+
+let data;
 // Event listener for button to display alert when clicked
 $('#btnSubmit').click(function() {
-    let data = $('#input').val();
-    alert(data);
-    $('.container').append($('<div><h2></h2></div>'));
-    $('h2').append(data);
+    alert($('#input').val());
+
+    data = $('#input').val();
+    $('h2').text(data);
+    $('h2').hover(
+        function() {
+            $( this ).addClass( "hover" );
+            $('.hover').css({'background-color': 'orange', 'border-radius': '5px'});
+        },
+            function() {
+                $( this ).removeClass( "hover" );
+              }
+        );
+    event.preventDefault();
+    
 });
+
+$('.container').append($('<div class="list"><h2>' + $('#input').val() + '</h2></div>'));
+
+
