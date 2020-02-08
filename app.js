13ca12a8275045ac2de67d3cdb6120db9d8112af
Ben Bryant <brbryant2639@gmail.com>
2020-02-08 11:08:16 -0600

    added ul, li's, click, dblclick

--- app.js
+++ app.js
@@ -15,20 +15,47 @@ $('#btnSubmit').click(function() {
     alert($('#input').val());
 
     data = $('#input').val();
-    $('h2').text(data);
-    $('h2').hover(
-        function() {
-            $( this ).addClass( "hover" );
-            $('.hover').css({'background-color': 'orange', 'border-radius': '5px'});
-        },
-            function() {
-                $( this ).removeClass( "hover" );
-              }
-        );
+    // Creates item element and appends input text to it
+    let item = $('<li>');
+    $(item).text(data);
+    $('ul').append($(item));
+    // $('h2').text(data);
+    // $('h2').hover(
+    //     function() {
+    //         $( this ).addClass( "hover" );
+    //         $('.hover').css({'background-color': 'orange', 'border-radius': '5px'});
+    //     },
+    //         function() {
+    //             $( this ).removeClass( "hover" );
+    //             $(this).css({'background-color': 'white'})
+    //           }
+    //     );
+    
+    //Sets variables, and functions to generate random numbers for colors                  
+    let r, g, b;
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+      }
+
+    // Changes the color of item when clicked
+    $('li').click(function(){
+        r = getRandomInt(255);
+        g = getRandomInt(255);
+        b = getRandomInt(255);
+        $(this).css('background-color', 'rgb(' + r + ',' + g + ',' + b +')');
+        // console.log('rgb(' + r + ' ' +g+ ' ' + b + ')');
+    });
+
+    // Removes item when double clicked
+    $('li').dblclick(function(){
+        $(this).remove();
+    });
+
     event.preventDefault();
     
 });
 
-$('.container').append($('<div class="list"><h2>' + $('#input').val() + '</h2></div>'));
+// Attaches new div, h2 and input to div.container
+// $('.container').append($('<div class="list"><h2>' + $('#input').val() + '</h2></div>'));
 
-
+$('.container').append($('<ul class="col my-3"></ul>'));
